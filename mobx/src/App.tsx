d94e0b58913e6988f81ac2c3fe82f63abca6de5d
PAPICO <ocipap0531@gmail.com>
2022-01-09 15:07:34 +0900

    react router 적용

--- mobx/src/App.tsx
+++ mobx/src/App.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
-import './App.css';
 import SortableTest from './SortableTest';
 import './decorator';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import AwesomeForm from './AwesomeForm';
 
 function App() {
-  return <SortableTest />;
+  return (
+    <BrowserRouter>
+      <ul>
+        <li>
+          <Link to="/sortable">Sortable</Link>
+        </li>
+        <li>
+          <Link to="/form">Form</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/sortable" element={<SortableTest />} />
+        <Route path="/form" element={<AwesomeForm />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
